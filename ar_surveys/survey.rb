7cab2b65e6ab4b9bbfabafe789cb1cb16544d48e
Hembaba & Jackson Behan <pair+Hembaba.jacksonbehan@devbootcamp.com>
2014-03-28 15:33:19 -0700

    changes to view

--- ar_surveys/survey.rb
+++ ar_surveys/survey.rb
@@ -18,7 +18,10 @@ class SurveyController
       if @user
          SurveyView.options
       else
-         SurveyView.no_user #returns blank or q
+         SurveyView.no_user
+         unless @option == "q"
+           SurveyController.run!
+          end#returns blank or q
       end
     end
 
@@ -41,13 +44,10 @@ class SurveyController
     end
   end
 
-
-
-
-    def self.fill_out_survey(survey_data)
-      survey_data[:user_id] = User.find_by_name(@name).id
-      Survey.create(survey_data)
-    end
+  def self.fill_out_survey(survey_data)
+    survey_data[:user_id] = User.find_by_name(@name).id
+    Survey.create(survey_data)
+  end
 
 end
 
@@ -57,27 +57,51 @@ class SurveyView
 
     def welcome_and_get_user
       self.welcome
+      puts ""
+      puts ""
       self.decoration
       self.prompt_for_username
     end
 
     def decoration
-       puts "*"*75
+       puts "-----"*8
     end
 
-    def welcome
-       puts "Welcome to Surveys"
-    end
+  def welcome
+       2.times do
+       puts "\e[H\e[2J"
+       puts ""
+       print "Welcome"
+       sleep(0.2)
+       end
+
+       2.times do
+       puts "\e[H\e[2J"
+       puts ""
+       print "Welcome to"
+       sleep(0.2)
+       end
+
+       3.times do
+       puts "\e[H\e[2J"
+       puts ""
+       print "Welcome to 'AHA moments' app for our team"
+       puts ""
+       print "Jon   Jen   Hem   Jackson   Brick   Strand"
+
+       end
+  end
 
     def prompt_for_username
-       print "Enter Username: "
-       @name = gets.chomp
+      puts ""
+      print "Who are You ? "
+      @name = gets.chomp
     end
 
     def list_user_surveys(user_surveys)
         decoration
         user_surveys.each do |survey|
-                   puts "Survey ID: #{survey.id}"
+          puts "Survey ID: #{survey.id}"
           puts "Confidence: #{survey.confidence}"
           puts "Aha: #{survey.aha}"
           decoration
@@ -86,10 +110,14 @@ class SurveyView
     end
 
     def new_survey
-      puts "Aha moment:"
+      puts "\e[H\e[2J"
+      puts ""
+      print "Your Aha moment:"
       aha = gets.chomp
-      puts "confidence level from 1-5"
+      puts ""
+      print "Confidence level from 1-5: "
       confidence = gets.chomp
+      puts ""
       result = {:aha => aha, :confidence => confidence.to_i}
 
       # result[:user_id] = User.find_by_name(@name).id
@@ -97,16 +125,21 @@ class SurveyView
     end
 
     def options
-     puts "1. See past survey results"
-     puts "2. Take a survey"
+     puts "\e[H\e[2J"
+     puts ""
+     puts "1. See past AHA moments"
+     puts "2. Save your AHA moment"
      puts "q to Exit"
      print "Choose an option: "
      @choice = gets.chomp
     end
 
     def no_user
+      puts "\e[H\e[2J"
+      puts ""
       puts "username does not exist. press enter to try again or 'q' to quit."
       @option = gets.chomp
+      # SurveyController.run!
     end
   end
 end
